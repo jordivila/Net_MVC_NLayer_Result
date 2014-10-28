76005b7c80492c04c8cc4a3af1d00db057aa2ddc
Jordi Vila <jordi.vila@gmail.com>
2014-10-28 13:00:06 +0100

    remove previous site

---
+++ VsixMvcAppResult/VsixMvcAppResult.UI.Web.Standalone_/Areas/LogViewer/Controllers/LogViewerController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+using Microsoft.Practices.Unity;
+using VsixMvcAppResult.Models.Enumerations;
+using VsixMvcAppResult.Models.Logging;
+using VsixMvcAppResult.Models.Unity;
+using VsixMvcAppResult.Resources.General;
+using VsixMvcAppResult.UI.Web.Areas.LogViewer.Models;
+using VsixMvcAppResult.UI.Web.Areas.UserAccount;
+using VsixMvcAppResult.UI.Web.Controllers;
+using VsixMvcAppResult.UI.Web.Common.Mvc.Html;
+using System.Diagnostics;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+using System.Reflection;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace VsixMvcAppResult.UI.Web.Areas.LogViewer.Controllers
+{
+    [VsixMvcAppResult.UI.Web.Common.Mvc.Attributes.Authorize(Roles = SiteRoles.Administrator)]
+    public class LogViewerController : Controller, IControllerWithClientResources
+    {
+        public string[] GetControllerJavascriptResources
+        {
+            get { return new string[0]; }
+        }
+        public string[] GetControllerStyleSheetResources
+        {
+            get { return new string[0]; }
+        }
+
+        private const string LogginConfigurationSectionName = "loggingConfiguration";
+
+        public ActionResult LogViewerByModel(LogViewerModel model)
+        {
+            if (WebGrid<LogMessageModel, LogViewerModel, DataFilterLogger>.IsWebGridEvent())
+            {
+                this.ModelState.Clear();
+                model.Filter = (DataFilterLogger)WebGrid<LogMessageModel, LogViewerModel, DataFilterLogger>.GetDataFilterFromPost();
+                model.Filter.IsClientVisible = false;
+            }
+
+            model.BaseViewModelInfo.Title = GeneralTexts.LogViewer;
+
+            LogWriterFactory logWriterFactory = new LogWriterFactory();
+
+            using (LogWriter logWriterInstance = logWriterFactory.Create())
+            {
+                using (TraceListener traceListenerInstance =
+                                logWriterInstance.TraceSources
+                                .Select(x => x.Value)
+                                .SelectMany(x => x.Listeners)
+                                .Where(x=>x.Name == model.Filter.LogTraceListenerSelected)
+                                .First())
+                {
+
+                    if (traceListenerInstance is ICustomTraceListener)
+                    {
+                        model.LogMessages = ((ICustomTraceListener)traceListenerInstance).SearchLogMessages(LogginConfigurationSectionName, model.Filter);
+                        return View(LogViewerViewHelper.LogViewerDisplay, model);
+                    }
+                    else
+                    {
+                        throw new Exception("TraceListener Not Supported");
+                    }
+                }
+            }
+        }
+
+        public ActionResult LogViewerById(string guid)
+        {
+            LogViewerModel model = new LogViewerModel();
+
+            using (ILoggingProxy provider = DependencyFactory.Resolve<ILoggingProxy>())
+            {
+                model.BaseViewModelInfo.Title = GeneralTexts.LogViewer;
+                model.LogMessages = new DataResultLogMessageList()
+                {
+                    Data = provider.LoggingExceptionGetById(Guid.Parse(guid)).Data
+                };
+            }
+
+
+            return View(LogViewerViewHelper.LogViewerById, model);
+        }
+
+        public ActionResult LogViewerByDatabaseTraceListener()
+        {
+            LogViewerModel model = new LogViewerModel();
+            model.BaseViewModelInfo.Title = GeneralTexts.LogViewer;
+            model.LogTraceSources = (ConfigurationManager.GetSection(LogginConfigurationSectionName) as LoggingSettings).TraceSources;
+
+            //string sourceName = "WCFGeneral"; // string.Empty;
+            //string sourceName = string.Empty;
+            string listenerName = "Database Trace Listener";// string.Empty;
+
+            //if (!string.IsNullOrEmpty(sourceName))
+            //{
+                //if (!model.LogTraceSources.Contains(sourceName))
+                //{
+                //    throw new Exception("Trace Data Source Not Found");
+                //}
+
+                model.Filter = new DataFilterLogger()
+                {
+                    //LogTraceSourceSelected = sourceName,
+                    CreationDate = DateTime.Now,
+                    //CreationDateTo = DateTime.Now,
+                    IsClientVisible = true,
+                    Page = 0,
+                    PageSize = (int)PageSizesAvailable.RowsPerPage10,
+                    SortAscending = true,
+                    SortBy = string.Empty
+                };
+
+                //model.LogTraceListeners = model.LogTraceSources.Where(x => x.Name == sourceName).First().TraceListeners;
+
+                //if (!string.IsNullOrEmpty(listenerName))
+                //{
+                    model.Filter.LogTraceListenerSelected = listenerName;
+                    //if (!model.LogTraceListeners.Contains(listenerName))
+                    //{
+                    //    throw new Exception("Trace Data Source Not Found");
+                    //}
+                //}
+            //}
+            
+            return View(LogViewerViewHelper.LogViewerDisplay, model);
+        }
+    }
+}
---
+++ VsixMvcAppResult/VsixMvcAppResult.UI.Web.Standalone_/Models/MenuModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using VsixMvcAppResult.Models;
+using VsixMvcAppResult.Models.Enumerations;
+
+namespace VsixMvcAppResult.UI.Web.Models
+{
+    public class MenuModel : baseModel
+    {
+        public MenuModel()
+        {
+            this.MenuItems = new List<MenuItemModel>();
+        }
+        public List<MenuItemModel> MenuItems { get; set; }
+        public IHtmlString Render(object htmlAttributes = null)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            ul.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+
+            foreach (var item in this.MenuItems)
+            {
+                ul.InnerHtml += item.Render();
+            }
+            return MvcHtmlString.Create(ul.ToString(TagRenderMode.Normal));
+        }
+
+    }
+    public class MenuItemModel : baseModel
+    {
+        public MenuItemModel()
+        {
+            this.Childs = new List<MenuItemModel>();
+            this.RolesAllowed = new List<SiteRoles>();
+        }
+
+        public MenuItemModel(string dataAction, string description, List<SiteRoles> rolesAllowed, List<MenuItemModel> childs)
+        {
+            this.DataAction = dataAction;
+            this.Description = description;
+            this.RolesAllowed = rolesAllowed == null ? new List<SiteRoles>() : rolesAllowed;
+            this.Childs = childs == null ? new List<MenuItemModel>() : childs;
+        }
+
+        public string DataAction { get; set; }
+        public string Description { get; set; }
+        public List<SiteRoles> RolesAllowed { get; set; }
+        public List<MenuItemModel> Childs { get; set; }
+        public IHtmlString Render()
+        {
+            TagBuilder li = new TagBuilder("li");
+            TagBuilder link = new TagBuilder("a");
+            link.SetInnerText(this.Description);
+            if (!string.IsNullOrEmpty(this.DataAction))
+            {
+                li.Attributes.Add("data-action", this.DataAction);
+                link.Attributes.Add("href", this.DataAction);
+            }
+            else
+            {
+                link.Attributes.Add("href", "javascript:void(0);");
+            }
+
+            li.InnerHtml += link.ToString(TagRenderMode.Normal);
+
+            if (this.Childs.Count > 0)
+            {
+                TagBuilder ul = new TagBuilder("ul");
+                foreach (var item in this.Childs)
+                {
+                    ul.InnerHtml += item.Render();
+                }
+                li.InnerHtml += ul.ToString(TagRenderMode.Normal);
+            }
+
+            return MvcHtmlString.Create(li.ToString(TagRenderMode.Normal));
+
+
+        }
+    }
+}
